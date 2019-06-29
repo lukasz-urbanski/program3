8ceb65a695fd91b88a39cf2eb2ebec9e010b74e5
Lukasz Urbanski <lukasz.urbanski@yahoo.pl>
2019-06-29 16:49:45 +0200

    Saturday EOB

--- Program3.Console/Program.cs
+++ Program3.Console/Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
+using Functions;
 
 namespace Program3
 {
@@ -10,6 +8,48 @@ namespace Program3
     {
         static void Main(string[] args)
         {
+            //Console.Write("Podaj liczbę: ");
+            //string num = (Console.ReadLine());
+            //Console.Write("Podaj liczbę sekund: ");
+            //int sleep = Int32.Parse(Console.ReadLine());
+            //Counter counter = new Counter(new WordToNumberConverter(num).getNumberAsInt(), "second");
+
+            TimerCallback callback = new TimerCallback(Tick);
+
+            Console.WriteLine("Creating timer: {0}\n",
+                               DateTime.Now.ToString("h:mm:ss"));
+
+            // create a one second timer tick
+            Timer stateTimer = new Timer(callback, null, 0, 1000);
+
+            //// loop here forever
+            //for (; ; )
+            //{
+            //    // add a sleep for 100 mSec to reduce CPU usage
+            //    Thread.Sleep(100);
+
+            //}
+
+            TimerCallback callback2 = new TimerCallback(Tick2);
+
+
+            //// loop here forever
+            //for (; ; )
+            //{
+            //    // add a sleep for 100 mSec to reduce CPU usage
+            //    Thread.Sleep(100);
+
+            //}
+            Console.ReadKey();
+
+        }
+        static public void Tick(Object stateInfo)
+        {
+            Console.WriteLine("Tick1: {0}", DateTime.Now.ToString("h:mm:ss"));
+        }
+        static public void Tick2(Object stateInfo)
+        {
+            Console.WriteLine("Tick2: {0}", DateTime.Now.ToString("h:mm:ss"));
         }
     }
 }
--- Program3.Functions/WordToNumberConverter.cs
+++ Program3.Functions/WordToNumberConverter.cs
@@ -2,42 +2,54 @@
 
 namespace Functions
 {
-    class WordToNumberConverter
+    public class WordToNumberConverter
     {
         private int NumberAsInt { get; set; }
-        private Dictionary<int, string> translator = new Dictionary<int, string>();
+
+        public int getNumberAsInt()
+        {
+            return this.NumberAsInt;
+        }
+
+        private Dictionary<string, int> translator = new Dictionary<string, int>();
 
         public WordToNumberConverter(string numberImputAsString)
         {
-            translator.Add(1, "one");
-            translator.Add(2, "two");
-            translator.Add(3, "three");
-            translator.Add(4, "four");
-            translator.Add(5, "five");
-            translator.Add(6, "six");
-            translator.Add(7, "seven");
-            translator.Add(8, "eight");
-            translator.Add(8, "eight");
-            translator.Add(9, "eight");
-            translator.Add(10, "ten");
-            translator.Add(11, "eleven");
-            translator.Add(12, "twelve");
-            translator.Add(13, "thirteen");
-            translator.Add(14, "fourteen");
-            translator.Add(15, "fifteen");
-            translator.Add(16, "sixteen");
-            translator.Add(17, "seventeen");
-            translator.Add(18, "eighteen");
-            translator.Add(19, "nineteen");
-            translator.Add(20, "twenty");
-            translator.Add(30, "thirty");
-            translator.Add(40, "fourty");
-            translator.Add(50, "fifty");
-            translator.Add(60, "sixty");
-            translator.Add(70, "seventy");
-            translator.Add(80, "eighty");
-            translator.Add(90, "ninety");
-            translator.Add(100, "one hundred");
+            string[] numberImputAsStringSplitted = numberImputAsString.Split(' ');
+            int tempNumberAsInt = 0;
+            translator.Add("one", 1);
+            translator.Add("two", 2);
+            translator.Add("three", 3);
+            translator.Add("four", 4);
+            translator.Add("five", 5);
+            translator.Add("six", 6);
+            translator.Add("seven", 7);
+            translator.Add("eight", 8);
+            translator.Add("nine", 9);
+            translator.Add("ten", 10);
+            translator.Add("eleven", 11);
+            translator.Add("twelve", 12);
+            translator.Add("thirteen", 13);
+            translator.Add("fourteen", 14);
+            translator.Add("fifteen", 15);
+            translator.Add("sixteen", 16);
+            translator.Add("seventeen", 17);
+            translator.Add("eighteen", 18);
+            translator.Add("nineteen", 19);
+            translator.Add("twenty", 20);
+            translator.Add("thirty", 30);
+            translator.Add("fourty", 40);
+            translator.Add("fifty", 50);
+            translator.Add("sixty", 60);
+            translator.Add("seventy,", 70);
+            translator.Add("eighty", 80);
+            translator.Add("ninety", 90);
+            translator.Add("hundred", 100);
+            for (int i = numberImputAsStringSplitted.Length - 1; i >= 0; i--)
+            {
+                tempNumberAsInt += translator[numberImputAsStringSplitted[i]];
+            }
+            this.NumberAsInt = tempNumberAsInt;
         }
     }
 }
